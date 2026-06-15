fa58caf377e8023dc873d4041fed1bee9ebf2613
SunilKotte <sunilkotte0909@gmail.com>
2026-06-15 16:20:25 -0500

    started writing backend with mongodb connection pointing local, wrote two routes to talk to db

---
+++ server/routes/connection.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	MongoDB := "mongodb://localhost:27017/caloriesdb"
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MongoDB))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected to Mongodb")
+	return client
+}
+
+
+var Client *mongo.Client= DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection{
+	var collection *mongo.Collection = client.Database("caloriesdb").Collection(collectionName)
+	return collection
+}
