c99d067a2b441c31457b90eff0dd777e19484fec
SunilKotte <sunilkotte0909@gmail.com>
2026-06-16 16:06:46 -0500

    added other two funtion to add the entry, undate the enrty

--- server/routes/entries.go
+++ server/routes/entries.go
@@ -6,15 +6,40 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/SunilKotte/go-react-calorie-tracker-yt/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"k8s.io/apimachinery/pkg/api/validate"
+	"k8s.io/kube-openapi/pkg/validation/validate"
 )
 
 var entryCollection *mongo.Collection = OpenCollection(Client, "calories")
 
 func AddEntry(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var entry models.Entry
+
+	if err := c.BindJSON(&entry); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+	}
+	validationErr := validate.Struct(entry)
+	if validationErr != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": validationErr.Error()})
+		fmt.Println(validationErr)
+	}
+	entry.Id = primitive.NewObjectID()
+	result, internalErr := entryCollection.InsertOne(ctx, entry)
+	if internalErr != nil {
+		msg := fmt.Sprintf("order item was not created")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+		fmt.Println(internalErr)
+	}
+	defer cancel()
+	c.JSON(http.StatusOK, result)
+
 }
 
 func GetEntries(c *gin.Context) {
@@ -41,13 +66,59 @@ func GetEntries(c *gin.Context) {
 
 func GetEntryByID(c *gin.Context) {
 
+	EntryID := c.Params.ByName("id")
+	docID, _ := primitive.ObjectIDFromHex(EntryID)
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	var entry bson.M
+	if err := entryCollection.FindOne(ctx, bson.M{"_id": docID}).Decode(&entry); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+	}
+	defer cancel()
+	fmt.Println(entry)
+
+	c.JSON(http.StatusOK, entry)
 }
 
 func GetEntriesByIngredient(c *gin.Context) {
 
 }
 func UpdateEntry(c *gin.Context) {
+	entryID := c.Params.ByName("id")
+	docID, _ := primitive.ObjectIDFromHex(entryID)
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var entry models.Entry
 
+	if err := c.BindJSON(&entry); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+	}
+	validationErr := validate.Struct(entry)
+
+	if validationErr != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": validationErr.Error()})
+		fmt.Println(validationErr)
+	}
+
+	result, err := entryCollection.ReplaceOne(
+		ctx,
+		bson.M{"_id": docID},
+		bson.M{
+			"dish":        entry.Dish,
+			"fat":         entry.Fat,
+			"ingredinets": entry.Ingredients,
+			"calories":    entry.Calories,
+		},
+	)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+	}
+	defer cancel()
+	c.JSON(http.StatusOK, result.ModifiedCount)
 }
 func UpdateIngredient(c *gin.Context) {
 
